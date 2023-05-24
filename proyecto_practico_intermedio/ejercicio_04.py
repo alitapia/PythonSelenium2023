192fafba27b40ec6ddcc2c7fe2768cd6ce8806b6
alitapia <alina.tapia@gmail.com>
2023-05-23 20:55:25 -0600

    add proyecto_practico_intermedio_Ejercicio_04 May232023

--- proyecto_practico_intermedio/ejercicio_04.py
+++ proyecto_practico_intermedio/ejercicio_04.py
@@ -32,7 +32,8 @@ class TestLaboratorioQAMinds:
         select_syncmaster.click()
         value_dolars = self.driver.find_element(By.XPATH, "//li//h2")
         dolar_price = value_dolars.text
-        dolar_price_amount = dolar_price[1:]
+        dolar_price = dolar_price[1:]
+        dolar_price_amount = float(dolar_price)
 
         change_currency_button = self.driver.find_element(By.XPATH, "//form[@id='form-currency']//span")
         change_currency_button.click()
@@ -42,35 +43,13 @@ class TestLaboratorioQAMinds:
 
         value_euros = self.driver.find_element(By.XPATH, "//li/h2")
         euro_price = value_euros.text
-        euro_price_amount = euro_price[:-1]
+        euro_price = euro_price[:-1]
+        euro_price_amount = float(euro_price)
 
 
         #Validate euro price < dolar price
         assert euro_price_amount < dolar_price_amount
 
 
-
-        '''
-        # click on mac option on desktop menu
-        mac_option = self.driver.find_element(By.XPATH, "//a[contains(@href,'category&path=20_27')]")
-        mac_option.click()
-
-        # Validate product displayed
-        mac_title = self.driver.find_element(By.XPATH, "//div[@class='caption']//a[contains(@href,'product_id=41')]")
-        assert mac_title.text == exp_desktop_title
-
-        # Open product
-        mac_title.click()
-
-        #Add to cart
-
-        add_cart_button = self.driver.find_element(By.XPATH, "//div[@class='form-group']/button[@id='button-cart']")
-        add_cart_button.click()
-
-        # Validate cart is updated
-        cart_value = self.driver.find_element(By.XPATH, "//span[@id='cart-total']")
-        assert exp_cart_value == cart_value.text
-
-'''
     def teardown_method(self):
         self.driver.quit()
