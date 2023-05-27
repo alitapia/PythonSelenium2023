58e168249c34ce6c8494d2fd30964ea774b94e5f
alitapia <alina.tapia@gmail.com>
2023-05-27 15:36:58 -0600

    add proyecto_practico_intermedio_Ejercicio_02 May272023

--- proyecto_practico_intermedio/ejercicio_02.py
+++ proyecto_practico_intermedio/ejercicio_02.py
@@ -16,9 +16,13 @@ class TestLaboratorioQAMinds:
         # Variables de valor esperado
         exp_error_no_product_msg = 'There is no product that matches the search criteria.'
         exp_product_title_cinema = 'Apple Cinema 30"'
+        id_cinema = "42"
         exp_product_title_nano = 'iPod Nano'
+        id_nano = "36"
         exp_product_title_touch = 'iPod Touch'
+        id_touch = "32"
         exp_product_title_mac = 'MacBook Pro'
+        id_macpro = "45"
 
         # Busqueda Display
         search_input = self.driver.find_element(By.NAME, "search")
@@ -29,19 +33,28 @@ class TestLaboratorioQAMinds:
         search_button.click()
         no_product_msg = self.driver.find_element(By.XPATH, "//div[@id='content']/p[text()='There is no product that matches the search criteria.']")
         assert exp_error_no_product_msg in no_product_msg.text
+
         # search in descriptions
         description_checkbox = self.driver.find_element(By.XPATH, "//input[@id='description']")
         description_checkbox.click()
         search_button_secondary = self.driver.find_element(By.XPATH, "//input[@id='button-search']")
         search_button_secondary.click()
-        cinema = self.driver.find_element(By.XPATH, "//div[@class='caption']//a[contains(@href,'product_id=42')]")
-        assert cinema.text == exp_product_title_cinema
-        nano = self.driver.find_element(By.XPATH, "//div[@class='caption']//a[contains(@href,'product_id=36')]")
-        assert nano.text == exp_product_title_nano
-        touch = self.driver.find_element(By.XPATH, "//div[@class='caption']//a[contains(@href,'product_id=32')]")
-        assert touch.text == exp_product_title_touch
-        mac = self.driver.find_element(By.XPATH, "//div[@class='caption']//a[contains(@href,'product_id=45')]")
-        assert mac.text == exp_product_title_mac
+
+        # Validate function
+        def validate_product(productid: str, expected_value: str):
+            current_value = self.driver.find_element(By.XPATH, f"//div[@class='caption']//a[contains(@href,'product_id={productid}')]")
+            return current_value.text
+
+
+        # validate displayed products
+        result = validate_product(id_cinema, exp_product_title_cinema)
+        assert result == exp_product_title_cinema
+        result = validate_product(id_nano, exp_product_title_nano)
+        assert result == exp_product_title_nano
+        result = validate_product(id_touch, exp_product_title_touch)
+        assert result == exp_product_title_touch
+        result = validate_product(id_macpro, exp_product_title_mac)
+        assert result == exp_product_title_mac
 
     def teardown_method(self):
         self.driver.quit()
